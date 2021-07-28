e323ee1e34d8a9d42c60007efac1505a910df712
Chris0710 <chris_lee_jing@163.com>
2021-07-28 14:57:06 +0800

    Update const_ns.go

--- clientset-demo/constant/const_ns.go
+++ clientset-demo/constant/const_ns.go
@@ -1,7 +1,6 @@
 package constant
 
 const (
-	// KubeConfigHomeDir   = "E:/Code/Go/go_books_resource_code/k8s-resource-code-analysis/"
 	NginxNamespace      = "nginx"
 	KubeSystemNamespace = "kube-system"
 )
